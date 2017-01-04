b018fce29632d0d5e9f2b49f72bd7ccc34576ad0
Rémy Oudompheng <remy@archlinux.org>
2017-01-04 22:47:58 +0100

    extramath: Fix assembly syntax, explain purpose of package.

--- extramath/math_amd64.s
+++ extramath/math_amd64.s
@@ -2,7 +2,7 @@
 TEXT ·DivmodU64(SB),7,$0
         MOVQ $0, DX
         MOVQ a+0(FP), AX
-        DIVQ b+8(FP),
+        DIVQ b+8(FP)
         MOVQ AX, quo+16(FP)
         MOVQ DX, rem+24(FP)
         RET
@@ -10,7 +10,7 @@ TEXT ·DivmodU64(SB),7,$0
 // func Mul(a, b uint64) (hi, lo uint64)
 TEXT ·MulU64(SB),7,$0
         MOVQ a+0(FP), AX
-        MULQ b+8(FP),
+        MULQ b+8(FP)
         MOVQ DX, hi+16(FP)
         MOVQ AX, lo+24(FP)
         RET
@@ -18,7 +18,7 @@ TEXT ·MulU64(SB),7,$0
 // func MulI64(a, b int64) (hi, lo int64)
 TEXT ·MulI64(SB),7,$0
         MOVQ a+0(FP), AX
-        IMULQ b+8(FP),
+        IMULQ b+8(FP)
         MOVQ DX, hi+16(FP)
         MOVQ AX, lo+24(FP)
         RET
